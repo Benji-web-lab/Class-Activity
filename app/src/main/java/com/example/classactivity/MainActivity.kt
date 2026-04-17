6e54861d458f191b91b8b1bce00cc2ed90599b03
Benji-web-lab <benginobenjamim007@gmail.com>
2026-04-17 11:48:27 +0200

    added song name

--- app/src/main/java/com/example/classactivity/MainActivity.kt
+++ app/src/main/java/com/example/classactivity/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.classactivity.Song
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // accessing the song class to populate song objects  with specific attributes
-        var song1 = Song("Pop Style","Drake", 2016,)
-        var song2 = Song("We Paid", "Lil Baby", 2022 )
-        var song3 = Song("That's That", "Sheff G", 2023)
+        var song1 = Song("Pop Style","Drake", 2016,"")
+        var song2 = Song("We Paid", "Lil Baby", 2022,"" )
+        var song3 = Song("That's That", "Sheff G")
 
-        
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/example/classactivity/Song.kt
+++ app/src/main/java/com/example/classactivity/Song.kt
@@ -1,6 +1,6 @@
 package com.example.classactivity
 
-import android.util.log
+import android.util.Log
 
 class Song (songName: String, songArtist: String, songYear: Int, songDuration: String){
       //created variables to hold song object's attributes
@@ -12,5 +12,18 @@ class Song (songName: String, songArtist: String, songYear: Int, songDuration: S
        init{
 
        }
+      //function to play song
+      fun Play() {
+       Log.v("song", "$name is playing.")
+      }
 
-     //
+
+     //function to pause song
+     fun Pause(){
+         Log.v("song", "$name is paused.")
+      }
+
+     //function to stop song
+     fun Stop(){
+         Log.v("song","$name is stopping")
+     }
