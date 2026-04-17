35609aabc8e60b4565e0b66f9286751efb49942a
Benji-web-lab <benginobenjamim007@gmail.com>
2026-04-17 11:30:30 +0200

    ClassActivity

---
+++ app/src/main/java/com/example/classactivity/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.classactivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // accessing the song class to populate song objects  with specific attributes
+        var song1 = Song("Pop Style","Drake", 2016,)
+        var song2 = Song("We Paid", "Lil Baby", 2022 )
+        var song3 = Song("That's That", "Sheff G", 2023)
+
+        
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/classactivity/Song.kt
@@ -0,0 +1,16 @@
+package com.example.classactivity
+
+import android.util.log
+
+class Song (songName: String, songArtist: String, songYear: Int, songDuration: String){
+      //created variables to hold song object's attributes
+       var name: String = ""
+       var artist: String = ""
+       var year: Int= 0
+       var duration: String = ""
+
+       init{
+
+       }
+
+     //
