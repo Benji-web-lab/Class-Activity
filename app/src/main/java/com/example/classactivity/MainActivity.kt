3ab925dabbcd4d84dc94d06f0ab299ddfc571c21
Benji-web-lab <benginobenjamim007@gmail.com>
2026-04-20 15:43:31 +0200

    added array for songs and loop

--- app/src/main/java/com/example/classactivity/MainActivity.kt
+++ app/src/main/java/com/example/classactivity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.classactivity
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,11 +14,23 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        var songShowtxt = findViewById<TextView>(R.id.SongsView)
+        var songDisplay = ""
+
         // accessing the song class to populate song objects  with specific attributes
         var song1 = Song("Pop Style","Drake", 2016,"")
         var song2 = Song("We Paid", "Lil Baby", 2022,"" )
         var song3 = Song("That's That", 'Sheff G')
 
+        //creating an array to hold the song objects
+        var songs = arrayOf<Song>(song1, song2, song3)
+
+        // for loop to loop through and display songs from the songs  array
+         for (song in songs){
+             songDisplay += "${song.Displaysong()}\n\n"
+         }
+
+            songShowtxt.text = songDisplay
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
